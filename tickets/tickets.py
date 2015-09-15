37ab24cefdcdab85d0694b9deb6351ce1c07a7c3
aramourad1 <ara.mourad@elev.it-gymnasiet.se>
2015-09-15 10:09:24 +0200

    hej :)

--- tickets/tickets.py
+++ tickets/tickets.py
@@ -1,3 +1,25 @@
+def ticket_price (age):
+
+    if age >= 0 and age <= 17:
+
+        return "det kostar 10kr"
+
+    elif age >= 18 and age <= 64:
+
+        return "det kostar 20kr"
+
+    elif age >= 65:
+
+        return "det kostar 15kr"
+
+
+
+
+
+
+
+
+
 """
 
 document your module here
--- tickettest.py
+++ tickettest.py
@@ -1,6 +1,7 @@
-#from ticket import yourfunction
+from tickets.tickets import ticket_price
 
 
+print ticket_price(10)
 
 
 #test your function here
